22067fec2513385c7e266024a2ae2ac2cd74b7f2
kwakdeung <51012589a@gmail.com>
2022-08-16 11:43:44 +0900

    Add heat

--- lib/components/lymphowear_state.dart
+++ lib/components/lymphowear_state.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:lymphowear_remote_app_logging_responsive/components/circularindicator.dart';
-import 'package:lymphowear_remote_app_logging_responsive/ui/responsive/responsive_builder.dart';
 
 class LymphoWearState extends StatelessWidget {
   const LymphoWearState({Key? key}) : super(key: key);
@@ -30,7 +29,7 @@ class LymphoWearState extends StatelessWidget {
                   child: ElevatedButton(
                     onPressed: () {},
                     style: ElevatedButton.styleFrom(
-                        side: const BorderSide(color: Colors.black),
+                        side: const BorderSide(color: Color(0xff212121)),
                         shape: const CircleBorder(), //<-- SEE HERE
                         padding: const EdgeInsets.all(10),
                         surfaceTintColor: Colors.white),
@@ -43,20 +42,20 @@ class LymphoWearState extends StatelessWidget {
                 ),
                 Container(
                   margin: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                  child: const CircularIndicator(),
+                  child: CircularIndicator(),
                 ),
                 Container(
                   margin: const EdgeInsets.fromLTRB(0, 96, 0, 16),
                   child: ElevatedButton(
                     onPressed: () {},
                     style: ElevatedButton.styleFrom(
-                        side: const BorderSide(color: Color(0xffE0E0E0)),
+                        side: const BorderSide(color: Color(0xff212121)),
                         shape: const CircleBorder(), //<-- SEE HERE
                         padding: const EdgeInsets.all(10),
                         surfaceTintColor: Colors.white),
                     child: const Icon(
                       Icons.add,
-                      color: Color(0xffE0E0E0),
+                      color: Colors.black,
                       size: 20,
                     ),
                   ),
